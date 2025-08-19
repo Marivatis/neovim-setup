4dbb04013215dee6d93c0e665ded06f785fd86ce
Marivatis <martyniuk.tech@gmail.com>
2025-08-19 15:02:40 +0300

    chore: add clipboard sync option

--- init.lua
+++ init.lua
@@ -2,16 +2,17 @@
 vim.g.mapleader = " "
 
 -- Basic configuration
-vim.opt.number = true          -- Нумерація рядків
-vim.opt.relativenumber = true  -- Відносна нумерація
-vim.opt.expandtab = true       -- Пробіли замість табів
-vim.opt.shiftwidth = 4         -- 4 пробіли при відступі
-vim.opt.tabstop = 4            -- ширина таба
-vim.opt.smartindent = true     -- авто-відступи
-vim.opt.wrap = false           -- не переносити довгі рядки
-vim.opt.ignorecase = true      -- пошук без урахування регістру…
-vim.opt.smartcase = true       -- …але якщо є великі букви – враховувати
-vim.opt.termguicolors = true   -- повна підтримка кольорів
+vim.opt.number = true          -- Line numbers
+vim.opt.relativenumber = true  -- Relative line numbers
+vim.opt.expandtab = true       -- Use spaces instead of tabs
+vim.opt.shiftwidth = 4         -- Indent size = 4 spaces
+vim.opt.tabstop = 4            -- Tab character width = 4
+vim.opt.smartindent = true     -- Smart auto-indentation
+vim.opt.wrap = false           -- Do not wrap long lines
+vim.opt.ignorecase = true      -- Case-insensitive search...
+vim.opt.smartcase = true       -- ...but case-sensitive if uppercase is used
+vim.opt.termguicolors = true   -- Enable 24-bit RGB colors
+vim.opt.clipboard = "unnamedplus"  -- sync with system clipboard
 
 -- Custom keymaps
 vim.keymap.set("n", "<leader>w", ":w<CR>")  -- Space+w = зберегти
