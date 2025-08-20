4a16293d650c034974a5e8a13f71268dd9f72f69
Marivatis <martyniuk.tech@gmail.com>
2025-08-20 16:48:38 +0300

    chore: add neo-tree.nvim keymap

--- init.lua
+++ init.lua
@@ -28,3 +28,7 @@ vim.cmd([[colorscheme everforest]])
 
 -- Custom keymaps
 vim.keymap.set("n", "<Leader>p", "o<Esc>p") -- Paste below on a new line
+
+-- Plugin custom keymaps
+vim.keymap.set("n", "<Leader>e", ":Neotree<CR>", { noremap = true, silent = true })        -- Go to neo-tree
+vim.keymap.set("n", "<Leader>E", ":Neotree toggle<CR>", { noremap = true, silent = true }) -- Open/Close neo-tree
