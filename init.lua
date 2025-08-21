ec1e3c7a4de78b29faf5921ee620588088990c89
Marivatis <martyniuk.tech@gmail.com>
2025-08-21 22:34:57 +0300

    refactor: update lazy setup

--- init.lua
+++ init.lua
@@ -2,25 +2,17 @@
 -- Make sure to setup `mapleader` and `maplocalleader` before
 -- loading lazy.nvim so that mappings are correct.
 vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
+vim.g.maplocalleader = ","
 
--- Add lazy.nvim initializator with path "nvim/config/lazy.lua"
+-- Add nvim default settings
+require("default")
+
+-- Add lazy.nvim initializator
 require("config.lazy")
 
 -- Add nvim-lspconfig initializator with path "nvim/config/lsp.lua"
 require("config.lsp")
 
--- Basic nvim configuration
-vim.opt.number = true         -- Line numbers
-vim.opt.relativenumber = true -- Relative line numbers
-vim.opt.expandtab = true      -- Use spaces instead of tabs
-vim.opt.shiftwidth = 4        -- Indent size = 4 spaces
-vim.opt.tabstop = 4           -- Tab character width = 4
-vim.opt.smartindent = true    -- Smart auto-indentation
-vim.opt.wrap = false          -- Do not wrap long lines
-vim.opt.ignorecase = true     -- Case-insensitive search...
-vim.opt.smartcase = true      -- ...but case-sensitive if uppercase is used
-vim.opt.termguicolors = true  -- Enable 24-bit RGB colors
 
 -- Lazy pluggins configuration
 vim.cmd([[colorscheme everforest]])
