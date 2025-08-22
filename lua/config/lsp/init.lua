fc175eacd1ca3c0316db5f2c6424fa61ea029370
Marivatis <martyniuk.tech@gmail.com>
2025-08-22 13:31:39 +0300

    fix: set correct lsp folder path

--- lua/config/lsp/init.lua
+++ lua/config/lsp/init.lua
@@ -1,9 +1,9 @@
--- Loads all /lua/lsp files
+-- Loads all /lua/config/lsp files
 
-local lsp_path = vim.fn.stdpath("config") .. "/lua/lsp"
+local lsp_path = vim.fn.stdpath("config") .. "/lua/config/lsp"
 for _, file in ipairs(vim.fn.readdir(lsp_path)) do
 	if file:match("%.lua$") then
-		local module_name = "lsp." .. file:gsub("%.lua$", "")
+		local module_name = "config.lsp." .. file:gsub("%.lua$", "")
 		pcall(require, module_name)
 	end
 end
