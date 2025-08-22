7bbdf9b2583593defb27bd61782bf6ec858fbdc1
Marivatis <martyniuk.tech@gmail.com>
2025-08-22 12:09:29 +0300

    chore: remove spell check settings

--- lua/settings.lua
+++ lua/settings.lua
@@ -1,9 +1,5 @@
 -- Basic nvim settings file
 
--- Enable spell checking
-vim.opt.spell = true
-vim.opt.spelllang = "en_us"
-
 -- Clipboard behavior. Set to "unnamed" or "unnamedplus" to use the system clipboard
 vim.opt.clipboard = ""
 
@@ -17,20 +13,20 @@ vim.opt.relativenumber = true
 
 -- Indentation behavior
 vim.opt.smartindent = true -- Smart auto-indentation
-vim.opt.expandtab = true   -- Use spaces instead of tabs
+vim.opt.expandtab = true -- Use spaces instead of tabs
 
 -- Search behavior
-vim.opt.hlsearch = false  -- Do not highlight all matches
+vim.opt.hlsearch = false -- Do not highlight all matches
 vim.opt.ignorecase = true -- Case-insensitive search
-vim.opt.smartcase = true  -- Case-sensitive if uppercase letters are used
+vim.opt.smartcase = true -- Case-sensitive if uppercase letters are used
 
 -- Visual settings
-vim.opt.cursorline = true   -- Highlight the current line
+vim.opt.cursorline = true -- Highlight the current line
 vim.opt.colorcolumn = "120" -- Highlight the 120th column
-vim.opt.scrolloff = 5       -- Keep 5 lines visible above/below the cursor
-vim.opt.sidescrolloff = 5   -- Keep 5 columns visible left/right of the cursor
+vim.opt.scrolloff = 5 -- Keep 5 lines visible above/below the cursor
+vim.opt.sidescrolloff = 5 -- Keep 5 columns visible left/right of the cursor
 
 -- Undo/Redo history settings
-vim.opt.undofile = true     -- Save undo history between sessions
-vim.opt.backup = false      -- Disable unnecessary backup files
+vim.opt.undofile = true -- Save undo history between sessions
+vim.opt.backup = false -- Disable unnecessary backup files
 vim.opt.writebackup = false -- Disable temporary write-backups
