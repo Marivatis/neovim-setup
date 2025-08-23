4ab625c1028b77076ad38f86fd013a9ffb248f4c
Marivatis <martyniuk.tech@gmail.com>
2025-08-23 17:24:03 +0300

    style: update format

--- lua/config/lsp/init.lua
+++ lua/config/lsp/init.lua
@@ -1,9 +1,9 @@
 -- Loads all /lua/config/lsp files
 
-local lsp_path = vim.fn.stdpath("config") .. "/lua/config/lsp"
+local lsp_path = vim.fn.stdpath("config") .. "/lua/config/lsp" -- lsp configs folder
 for _, file in ipairs(vim.fn.readdir(lsp_path)) do
-	if file:match("%.lua$") then
-		local module_name = "config.lsp." .. file:gsub("%.lua$", "")
-		pcall(require, module_name)
-	end
+   if file:match("%.lua$") then
+      local module_name = "config.lsp." .. file:gsub("%.lua$", "")
+      pcall(require, module_name)
+   end
 end
--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,5 +1,6 @@
 -- This file meant to store basic nvim custom keymaps
 
+-- Copy/Paste
 vim.keymap.set("n", "<Leader>p", '"+p')
 vim.keymap.set("n", "<Leader>pp", 'o<Esc>"+P')
 vim.keymap.set("v", "<Leader>p", '"+p')
@@ -7,6 +8,7 @@ vim.keymap.set("v", "<Leader>pp", 'o<Esc>"+P')
 vim.keymap.set("v", "<Leader>y", '"+y')
 vim.keymap.set("n", "<Leader>yy", '"+yy')
 
+-- LSP diagnostics
 vim.keymap.set("n", "<Leader>d", vim.diagnostic.open_float, { silent = true })
 vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { silent = true })
 vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { silent = true })
