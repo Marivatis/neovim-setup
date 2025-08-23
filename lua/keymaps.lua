50960af93f4ce3f4c938b1ee47d7231e4631c0ce
Marivatis <martyniuk.tech@gmail.com>
2025-08-23 16:29:08 +0300

    chore: update copy/paste keymaps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,9 +1,11 @@
 -- This file meant to store basic nvim custom keymaps
 
-vim.keymap.set("n", "<Leader>p", '"+p')       -- Paste from the system register
-vim.keymap.set("n", "<Leader>P", 'o<Esc>"+P') -- Paste the from system register below on a new line
-vim.keymap.set("v", "<Leader>y", '"+y')       -- Copy to the system register
-vim.keymap.set("n", "<Leader>Y", '"+yy')      -- Copy the line to the system register
+vim.keymap.set("n", "<Leader>p", '"+p')
+vim.keymap.set("n", "<Leader>pp", 'o<Esc>"+P')
+vim.keymap.set("v", "<Leader>p", '"+p')
+vim.keymap.set("v", "<Leader>pp", 'o<Esc>"+P')
+vim.keymap.set("v", "<Leader>y", '"+y')
+vim.keymap.set("n", "<Leader>yy", '"+yy')
 
 vim.keymap.set("n", "<Leader>d", vim.diagnostic.open_float, { silent = true })
 vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { silent = true })
